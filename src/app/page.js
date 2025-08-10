bcc779956cfc5aaa8447245d3bdc362f23f8742a
byeongkookkang <byeongguk1998@gmail.com>
2025-08-10 21:53:25 +0900

    몽고db 연결해서 목록페이지 생성

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,18 @@
-export default function Home(){
+import clientPromise from "../../utils/db";
+
+export default async function Home(){
+    const client = await clientPromise;
+    const db = client.db('community_forum'); // ✨ DB 이름을 입력하세요
+    const posts = await db.collection('post').find({}).toArray();
+    console.log(posts)
+
   return(
     <div>
-      메인 홈 화면
+      {posts.map((post,i)=>{
+        return(
+          <div key='i'>{post.title}</div>
+        )
+      })}
     </div>
   )
 }
