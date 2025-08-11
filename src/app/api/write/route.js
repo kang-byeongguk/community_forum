2837f5e9cbe7e5d64fa8662a546303a20f190ada
byeongkookkang <byeongguk1998@gmail.com>
2025-08-12 01:48:29 +0900

    create 서버기능에서 리다이렉트 기능 추가

--- src/app/api/write/route.js
+++ src/app/api/write/route.js
@@ -11,9 +11,10 @@ export async function POST(request) {
   const title=formData.get('title');
   const content=formData.get('content');
 
-   const client = await clientPromise;
-    const db = client.db('community_forum'); // ✨ DB 이름을 입력하세요
-    const posts = await db.collection('post').insertOne({title:title,content:content});
+  const client = await clientPromise;
+  const db = client.db('community_forum'); // ✨ DB 이름을 입력하세요
+  const posts = await db.collection('post').insertOne({title:title,content:content});
 
-  return NextResponse.json('처리완료함', { status: 200 })
+  // return NextResponse.json('처리완료함', { status: 200 })
+  return NextResponse.redirect(new URL('/', request.url));
 }
