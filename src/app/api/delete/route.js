d6a3a389b3edf2432b0f6ad67862676c06cbae84
byeongkookkang <byeongguk1998@gmail.com>
2025-08-12 17:50:23 +0900

    .

--- src/app/api/delete/route.js
+++ src/app/api/delete/route.js
@@ -8,12 +8,16 @@ export async function POST(request) {
 //   const title=formData.get('title');
 //   const content=formData.get('content');
 //   const id = formData.get('id');
+try{
 const {id} = await request.json()
-
-
   const client = await clientPromise;
   const db = client.db('community_forum'); // ✨ DB 이름을 입력하세요
   const posts = await db.collection('post').deleteOne({_id :new ObjectId(id)});
+}catch(error){
+    return NextResponse.json('에러발생', { status: 500 })
+
+}
+  
 
   return NextResponse.redirect(new URL('/', request.url));
 }
